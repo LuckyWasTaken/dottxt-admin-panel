5a04b50ec393f7bd2a3ad683010d1351663081c2
Nikolay Tikhonov <a.for.autism1337@gmail.com>
2018-08-18 19:59:13 +0300

    Done order updating routine

--- src/app/dashboard/store/dashboard.actions.ts
+++ src/app/dashboard/store/dashboard.actions.ts
@@ -4,6 +4,7 @@ import { Order } from '../../shared/order.model';
 export const SET_ORDERS = 'SET_ORDERS';
 export const PUSH_ORDERS = 'PUSH_ORDERS';
 export const TRY_GET_ORDERS = 'TRY_GET_ORDERS';
+export const TRY_UPDATE_ORDER = 'TRY_UPDATE_ORDER';
 
 export class SetOrders implements Action {
     readonly type = SET_ORDERS;
@@ -17,6 +18,12 @@ export class PushOrders implements Action {
 
 export class TryGetOrders implements Action {
     readonly type = TRY_GET_ORDERS;
+}
+
+export class TryUpdateOrder implements Action {
+    readonly type = TRY_UPDATE_ORDER;
+
+    constructor(public payload: {order: Order, id: number}) {}
 }
 
 export type DashboardActions = SetOrders | PushOrders | TryGetOrders;
