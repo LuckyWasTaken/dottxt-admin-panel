864a36fdfe4d6653970f668b243ac2f62e3b5508
Nikolay Tikhonov <a.for.autism1337@gmail.com>
2018-08-18 20:48:27 +0300

    Done navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AuthComponent } from './auth/auth.component';
 import { OrdersComponent } from './dashboard/orders/orders.component';
 import { OrderComponent } from './dashboard/order/order.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ItemsComponent } from './dashboard/items/items.component';
 
 const appRoutes: Routes = [
     {path: '', component: AuthComponent},
     {path: 'dashboard', component: DashboardComponent, children: [
         {path: 'orders', component: OrdersComponent},
-        {path: 'orders/:id', component: OrderComponent}
+        {path: 'orders/:id', component: OrderComponent},
+        {path: 'items', component: ItemsComponent}
     ]}
 ];
 
